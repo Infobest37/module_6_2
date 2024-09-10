b7e099f0a2645de2adfea853d0c82b8aa7f67619
Евгений <info-best@bk.ru>
2024-09-10 13:42:04 +0300

    second

--- module_6_2.py
+++ module_6_2.py
@@ -4,39 +4,42 @@ class Vehicle:
         self.__model = str(__model)
         self.__engine_power = int(__engine_power)
         self.__color = str(__color)
-        self._COLOR_VARIANTS = ["Синий", "Красный", "Желтый", "Оранжевый"]
+        self._COLOR_VARIANTS = ["Синий", "красный", "Желтый", "Оранжевый"]
 
 
     def get_model(self, __model):
-       self.__model = str(__model)
-       return self.__model
+       print(f"Модель: {self.__model}")
     def get_horsepower(self, __engine_power):
-        self.__engine_power = int(__engine_power)
-        return self.__engine_power
+        print(f"Мощьнось двигателя: {self.__engine_power}")
     def get_color(self, __color):
-        self.__color = str(__color)
-        return self.__color
-
+        print(f"Цвет: {self.__color}")
     def info(self):
-        print(f"Mодель: {self.__model}\nМощьность двигателя: {self.__engine_power}\nЦвет: {self.__color}\n"
-              f"Владелец: {self.owner}")
+             self.get_model(str(self.__model))
+             self.get_horsepower(str(self.__engine_power))
+             self.get_color(str(self.__color))
+             print(f"Выладелец: {self.owner}")
 
     def set_color(self, new_color):
-        self.__color = new_color.capitalize()
-        if new_color.capitalize() in self._COLOR_VARIANTS:
-            self._COLOR_VARIANTS.remove(new_color.capitalize())
 
-        else:
-            print(f"{new_color} цвет отсутствует в наличии")
+
+        for color in self._COLOR_VARIANTS:
+            if color.capitalize() == new_color.capitalize():
+                print(f"Урааа мы на конец-то перекрасили машину с {self.__color} на {new_color}")
+                self.__color = new_color
+                return
+        for color in self._COLOR_VARIANTS:
+            if color.capitalize() != new_color.capitalize():
+                print(f"{new_color} отсутвует в наличии")
+                return
+
+
 
 
 class Sedan(Vehicle):
-    def __init__(self, owner, __model, __engine_power, __color):
+    def __init__(self, owner, __model, __engine_power, __color, __PASSENGERS_LIMIT = 5):
         super().__init__(owner, __model, __engine_power, __color)
-        self._owner = str(owner)
-        self.__model = str(__model)
-        self.__engine_power = int(__engine_power)
-        self.__color = str(__color)
+        self.__PASSENGERS_LIMIT = int(__PASSENGERS_LIMIT)
+
 
 
 
@@ -49,7 +52,7 @@ vehicle1.info()
 
 # # Меняем свойства (в т.ч. вызывая методы)
 vehicle1.set_color('Розовый')
-vehicle1.set_color('красный')
+vehicle1.set_color('КРАСНЫЙ')
 vehicle1.owner = 'Vasyok'
 #
 # # Проверяем что поменялось
